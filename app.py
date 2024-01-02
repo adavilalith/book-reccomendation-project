92a87b3763a23a2d9ebd3548c96dfd5c65b95da5
Adavi Lalith <adavilalith11@gmail.com>
2024-01-03 03:13:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+popularity_df=pickle.load(open('popularity_df.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+scores = pickle.load(open('scores.pkl','rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_ui():
+    return render_template('index.html')
+
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    idx = np.where(pt.index==user_input)[0][0]
+    items = sorted(list(enumerate(scores[idx])),key = lambda x:x[1],reverse=True)[1:9]
+    data=[]
+    for i in items:
+        item=[]
+        temp = books[books['Book-Title']==pt.index[i[0]]]
+        item.extend(list(temp.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp.drop_duplicates('Book-Title')['Book-Author'].values))       
+        item.extend(list(temp.drop_duplicates('Book-Title')['Image-URL-M'].values))
+        data.append(item)
+    if not data[0]:
+        return "invalid name"
+    return render_template('index.html',data=data)
+
+@app.route('/top')
+def top_ui():
+    return render_template('top50.html',
+                           book_name = list(popularity_df['Book-Title'].values),
+                           author=list(popularity_df['Book-Author'].values),
+                           image=list(popularity_df['Image-URL-M'].values),
+                           no_ratings=list(popularity_df['num_ratings'].values),
+                           rating=list(popularity_df['avg_rating'].values)
+                           )
+
+if __name__ == '__main__':
+    app.run(debug=True)
